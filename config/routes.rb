96195a4f54efb29e094340bb8210bd9664632989
Alexander <alexcrisel4@gmail.com>
2020-01-20 11:18:40 -0800

    Adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'usersbundle/exec'
-  get 'usersbundle/rails'
-  get 'usersbundle/g'
-  get 'usersbundle/modle'
-  get 'usersbundle/session'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+    resources :users, only:[:create]
+    resource :session, only: [:create, :destroy, :show]
+
+     root "static_pages#root"
 end
